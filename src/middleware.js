40e015129108dfbaab032eb37587af47cbaf92e4
SangTrafic <146843231+SangTrafic@users.noreply.github.com>
2024-02-28 16:02:43 +0700

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,8 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(req) {
-    let ua = req.headers.get('user-agent');
-    if(ua.includes('facebookexternalhit') || ua.includes('facebookcatalog')){
-        return NextResponse.next();
-    } else {
+    if (req.headers.get('referer')?.includes('facebook.com')) {
         const targetURL = `${process.env.WORDPRESS_REDIRECT_DOMAIN}/${req.nextUrl.pathname.replace('/posts/', '')}`;
         return NextResponse.redirect(targetURL);
-    }   
+  }
 }
